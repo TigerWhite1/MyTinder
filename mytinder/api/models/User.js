a682148bc2ac30483d1f6e59418778ae7607a82d
dugue_t <dugue_t@epitech.eu>
2016-04-18 15:01:22 +0200

    Api login

--- mytinder/api/models/User.js
+++ mytinder/api/models/User.js
@@ -4,18 +4,34 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
-
+var bcrypt = require('bcrypt');
 module.exports = {
 
-  attributes: {
-  	firstName: 'STRING',
-  	lastName: 'STRING',
-  	age: { type: 'INTEGER'},
-  	birthDate: 'DATE',
-  	phoneNumber: {type: 'STRING'},
-  	emailAddress: {type: 'STRING'},
-	pseudo: {type: 'STRING'}
+	attributes: {
+		firstName: 'STRING',
+		lastName: 'STRING',
+		age: { type: 'INTEGER', max: 150,required: true},
+		birthDate: 'DATE',
+		phoneNumber: {type: 'STRING'},
+		emailAddress: {type: 'email', unique: true, required: true},
+		pseudo: {type: 'STRING', maxLength: 20, minLength: 5 },
+		password: {
+			type: 'string',
+			minLength: 6,
+			required: true
+			// columnName: 'hashed_password'
+		},
+		beforeCreate: function (values, cb) {
 
-  }
+    // Hash password
+    bcrypt.hash(values.password, 10, function(err, hash) {
+    	if(err) return cb(err);
+    	values.password = hash;
+      //calling cb() with an argument returns an error. Useful for canceling the entire operation if some criteria fails.
+      cb();
+  });
+}
+
+}
 };
 
