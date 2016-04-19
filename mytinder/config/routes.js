fe4fef88f4a2d009e7331430792c827a903d1209
dugue_t <dugue_t@epitech.eu>
2016-04-19 09:33:26 +0200

    Api auth1

--- mytinder/config/routes.js
+++ mytinder/config/routes.js
@@ -20,7 +20,7 @@
  * http://sailsjs.org/#!/documentation/concepts/Routes/RouteTargetSyntax.html
  */
 
-module.exports.routes = {
+ module.exports.routes = {
 
   /***************************************************************************
   *                                                                          *
@@ -34,7 +34,18 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+  'get /login': {
+   view: 'login'
+ },
+
+ 'post /login': 'AuthController.login',
+
+ '/logout': 'AuthController.logout',
+
+ 'get /signup': {
+  view: 'signup'
+}
 
   /***************************************************************************
   *                                                                          *
