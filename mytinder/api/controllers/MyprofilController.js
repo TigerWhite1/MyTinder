a891ae94d6550de11c109bac12ddb1d3f71c65bc
dugue_t <dugue_t@epitech.eu>
2016-04-21 20:10:04 +0200

    Api uploads image base64

---
+++ mytinder/api/controllers/MyprofilController.js
@@ -0,0 +1,14 @@
+/**
+ * MyprofilController
+ *
+ * @description :: Server-side logic for managing Myprofils
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+	myprofil: function(req, res) {
+	}
+
+};
+
